40164851fc1ba641842185a6f803cd9492ea892b
Vraj Patel <31vrajpatel@gmail.com>
2024-03-23 19:29:12 +0530

    github link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                 <Link to="/movies/top_rated"><span className='mx-5 py-2 text-xl cursor-pointer text-white hover:text-red-500'>Top Rated</span></Link>
                 <Link to="/movies/upcoming"><span className='mx-5 py-2 text-xl cursor-pointer text-white hover:text-red-500'>Upcoming</span></Link>
             </div>
-            <Link to="https://github.com/vrajkmrpatel" target='_blank' className='p-3'>
+            <Link to="https://github.com/vrajkmrpatel/imdbclone" target='_blank' className='p-3'>
             
             <FaGithub className='text-2xl cursor-pointer' />
             </Link>
